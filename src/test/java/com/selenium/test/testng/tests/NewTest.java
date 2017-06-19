c9e09d5afdc756d674b0be1bf2acc81e58e23bc3
mashundr <mariia.khlon@gmail.com>
2017-06-19 20:47:02 +0200

    first test commit

---
+++ src/test/java/com/selenium/test/testng/tests/NewTest.java
@@ -0,0 +1,14 @@
+package com.selenium.test.testng.tests;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NewTest {
+
+    @FindBy(id = "testidbutton")
+    private WebElement testButton;
+
+    public void clickTestButton(){
+        testButton.click();
+    }
+}
